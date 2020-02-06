30bfee0784c29c4df9e454b0ea017c28f6ade358
alexandra <le.onze.mai@gmail.com>
2020-02-06 21:02:05 +0300

    --fix create wizard settings window

--- js/setup.js
+++ js/setup.js
@@ -1,22 +1,26 @@
 'use strict';
 
+// Константы
+
 var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101,137,164)', 'rgb(241,43,107)', 'rgb(146,100,161)', 'rgb(56,159,117)', 'rgb(215,210,55)', 'rgb(0,0,0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var showDomElement = function (element) {
-  element.classList.remove('hidden');
-};
+// Селектора
 
 var userDialog = document.querySelector('.setup');
-
-showDomElement(userDialog);
-
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
+var setupSimilar = userDialog.querySelector('.setup-similar');
+
+// Функции
+
+var showDomElement = function (element) {
+  element.classList.remove('hidden');
+};
 
 var randomArrayItem = function (array) {
   var randomInteger = Math.random() * array.length;
@@ -34,8 +38,6 @@ var createNames = function (firstNamesArray, surnamesArray) {
   return names;
 };
 
-var wizardNames = createNames(WIZARD_FIRST_NAMES, WIZARD_SURNAMES);
-
 var createSimilarCharacters = function (names, cloth, eyes, length) {
   var characters = [];
 
@@ -46,9 +48,7 @@ var createSimilarCharacters = function (names, cloth, eyes, length) {
   return characters;
 };
 
-var wizards = createSimilarCharacters(wizardNames, WIZARD_COAT_COLORS, WIZARD_EYES_COLORS, 4);
-
-var renderWizard = function (wizard) {
+var createWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -58,12 +58,21 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-similarListElement.appendChild(fragment);
+var renderWizards = function (wizardsArray, container) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizardsArray.length; i++) {
+    fragment.appendChild(createWizard(wizardsArray[i]));
+  }
 
-var setupSimilar = userDialog.querySelector('.setup-similar');
+  container.appendChild(fragment);
+};
 
+// Вызовы функций
+
+showDomElement(userDialog);
+
+var wizardNames = createNames(WIZARD_FIRST_NAMES, WIZARD_SURNAMES);
+var wizards = createSimilarCharacters(wizardNames, WIZARD_COAT_COLORS, WIZARD_EYES_COLORS, 4);
+
+renderWizards(wizards, similarListElement);
 showDomElement(setupSimilar);
